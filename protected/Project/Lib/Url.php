33b9a2b9dbdb51be2cbdc5fd1fc49003d644e109
Syone <syone7@hotmail.com>
2014-03-19 16:18:43 +0100

    Update Url.php

--- protected/Project/Lib/Url.php
+++ protected/Project/Lib/Url.php
@@ -26,38 +26,4 @@ class Url {
 		return $_SERVER['PHP_SELF'] . (empty($params) ? '' : '?' . http_build_query($params));
 	}
 
-	public static function item($id, $slug = null) {
-		$params['i'] = $id;
-		if (!empty($slug)) $params['slug'] = $slug;
-		return self::build('item', null, $params);
-	}
-
-	public static function itemPicture($itemId, $typeId = null) {
-		if (file_exists(ITEM_PICTURE_DIR . '/' . $itemId)) {
-			return ITEM_PICTURE_ROOT . '/' . $itemId;
-		} elseif (!is_dir(__DIR__ . '/../../../assets/img/default-item-picture/' . $typeId)) {
-			return WEB_ROOT . '/assets/img/default-item-picture/itb';
-		} elseif (empty($typeId)) {
-			return WEB_ROOT . '/assets/img/default-item-picture/itb';
-		} else {
-			$files = scandir(__DIR__ . '/../../../assets/img/default-item-picture/' . $typeId);
-			$nbImg = count($files) - 2;
-			$imgId = $itemId%$nbImg + 1;
-			return WEB_ROOT . '/assets/img/default-item-picture/' . $typeId . '/' . $imgId;
-		}
-	}
-
-	public static function itemBackground($itemId, $typeId = null) {
-		if (empty($typeId)) {
-			return WEB_ROOT . '/assets/img/default-item-background/itb';
-		} elseif (!is_dir(__DIR__ . '/../../../assets/img/default-item-background/' . $typeId)) {
-			return WEB_ROOT . '/assets/img/default-item-background/itb';
-		} else {
-			$files = scandir(__DIR__ . '/../../../assets/img/default-item-background/' . $typeId);
-			$nbImg = count($files) - 2;
-			$imgId = $itemId%$nbImg + 1;
-			return WEB_ROOT . '/assets/img/default-item-background/' . $typeId . '/' . $imgId;
-		}
-	}
-
 }
