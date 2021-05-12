39cae851740e4a8435cdbffaf88438b111874db3
syone <syone7@gmail.com>
2021-05-13 00:11:44 +0200

    clean

--- protected/src/Component/Form/Element/CodeArea.php
+++ protected/src/Component/Form/Element/CodeArea.php
@@ -14,18 +14,18 @@ class CodeArea extends \Sy\Component\Html\Form\Textarea {
 	}
 
 	private function preInit() {
-		$this->addCssLink(CODE_MIRROR . '/lib/codemirror.css');
+		$this->addCssLink(CODE_MIRROR . '/lib/codemirror.min.css');
 		$this->addCssLink(CODE_MIRROR . '/theme/darcula.min.css');
-		$this->addJsLink(CODE_MIRROR . '/lib/codemirror.js', self::JS_BOTTOM);
-		$this->addJsLink(CODE_MIRROR . '/mode/htmlmixed/htmlmixed.js', self::JS_BOTTOM);
-		$this->addJsLink(CODE_MIRROR . '/mode/xml/xml.js', self::JS_BOTTOM);
-		$this->addJsLink(CODE_MIRROR . '/mode/javascript/javascript.js', self::JS_BOTTOM);
-		$this->addJsLink(CODE_MIRROR . '/mode/css/css.js', self::JS_BOTTOM);
-		$this->addJsLink(CODE_MIRROR . '/mode/clike/clike.js', self::JS_BOTTOM);
-		$this->addJsLink(CODE_MIRROR . '/mode/php/php.js', self::JS_BOTTOM);
-		$this->addJsLink(CODE_MIRROR . '/addon/edit/matchbrackets.js', self::JS_BOTTOM);
-		$this->addJsLink(CODE_MIRROR . '/addon/edit/closebrackets.js', self::JS_BOTTOM);
-		$this->addJsLink(CODE_MIRROR . '/addon/comment/comment.js', self::JS_BOTTOM);
+		$this->addJsLink(CODE_MIRROR . '/lib/codemirror.min.js');
+		$this->addJsLink(CODE_MIRROR . '/mode/htmlmixed/htmlmixed.min.js');
+		$this->addJsLink(CODE_MIRROR . '/mode/xml/xml.min.js');
+		$this->addJsLink(CODE_MIRROR . '/mode/javascript/javascript.min.js');
+		$this->addJsLink(CODE_MIRROR . '/mode/css/css.min.js');
+		$this->addJsLink(CODE_MIRROR . '/mode/clike/clike.min.js');
+		$this->addJsLink(CODE_MIRROR . '/mode/php/php.js');
+		$this->addJsLink(CODE_MIRROR . '/addon/edit/matchbrackets.min.js');
+		$this->addJsLink(CODE_MIRROR . '/addon/edit/closebrackets.min.js');
+		$this->addJsLink(CODE_MIRROR . '/addon/comment/comment.min.js');
 		$this->setContent(array("<?php\n"));
 	}
 
@@ -36,7 +36,7 @@ class CodeArea extends \Sy\Component\Html\Form\Textarea {
 		$js = new \Sy\Component();
 		$js->setTemplateFile(TPL_DIR . '/Component/Form/Element/CodeArea/CodeArea.js');
 		$js->setVar('CODE_AREA_ID', $this->getAttribute('id'));
-		$this->addJsCode($js->__toString(), self::JS_BOTTOM);
+		$this->addJsCode($js->__toString());
 	}
 
 }
